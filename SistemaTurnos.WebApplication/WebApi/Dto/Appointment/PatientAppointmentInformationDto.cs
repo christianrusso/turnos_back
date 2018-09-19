8c6649ffc27fa789fab502ea708264b98fe04c2e
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2018-09-19 12:12:20 -0300

    das

--- SistemaTurnos.WebApplication/WebApi/Dto/Appointment/PatientAppointmentInformationDto.cs
+++ SistemaTurnos.WebApplication/WebApi/Dto/Appointment/PatientAppointmentInformationDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SistemaTurnos.WebApplication.Database.Enums;
 
 namespace SistemaTurnos.WebApplication.WebApi.Dto.Appointment
 {
@@ -17,5 +18,6 @@ namespace SistemaTurnos.WebApplication.WebApi.Dto.Appointment
         public DateTime DateTime { get; set; }
 
         public int Id { get; set; }
+        public AppointmentStateEnum State { get; set; }
     }
 }
