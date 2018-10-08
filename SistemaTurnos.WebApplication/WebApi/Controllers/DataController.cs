421d0d3a507c1b336c6d2cd624dbf9f03cbe76df
Miguel Elias Arteaga Orozco <arteagamiguel@gmail.com>
2018-10-08 10:13:33 -0300

    Update DataController.cs
    rubro spacename

--- SistemaTurnos.WebApplication/WebApi/Controllers/DataController.cs
+++ SistemaTurnos.WebApplication/WebApi/Controllers/DataController.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using SistemaTurnos.WebApplication.WebApi.Services;
+using SistemaTurnos.WebApplication.Database.Enums;
 
 namespace SistemaTurnos.WebApplication.WebApi.Controllers
 {
