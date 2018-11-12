d81df63cfbb84210b735ce2cfa306ff21da7dd23
Miguel E arteaga Orozco <arteagamiguel@gmail.com>
2018-11-11 21:06:51 -0300

    RequestAppointmentByClientDto, RequestHairdressingAppointmentByClientDto

--- SistemaTurnos.WebApplication/WebApi/Dto/Appointment/RequestAppointmentByClientDto.cs
+++ SistemaTurnos.WebApplication/WebApi/Dto/Appointment/RequestAppointmentByClientDto.cs
@@ -8,25 +8,7 @@ namespace SistemaTurnos.WebApplication.WebApi.Dto.Appointment
         [Required]
         public int ClinicId { get; set; }
 
-        [Required]
-        [StringLength(50, MinimumLength = 4)]
-        public string FirstName { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 4)]
-        public string LastName { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 4)]
-        public string Address { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 4)]
-        public string PhoneNumber { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength = 4)]
-        public string Dni { get; set; }
+       
 
         public int MedicalPlanId { get; set; }
 
