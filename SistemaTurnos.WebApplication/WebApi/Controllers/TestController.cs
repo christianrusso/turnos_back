ed548b842026d0e41776d984a425dfa7ea2e6d93
Fernando Gomez <fernando.gmz12@gmail.com>
2018-12-04 19:20:34 -0300

    prueba concepto mercadopago

--- SistemaTurnos.WebApplication/WebApi/Controllers/TestController.cs
+++ SistemaTurnos.WebApplication/WebApi/Controllers/TestController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using MercadoPago;
+using MercadoPago.Resources;
+using MercadoPago.DataStructures.Preference;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using SistemaTurnos.WebApplication.WebApi.Dto.Email;
 using SistemaTurnos.WebApplication.WebApi.Services;
 using System.Collections.Generic;
+using MercadoPago.Common;
 
 namespace SistemaTurnos.WebApplication.WebApi.Controllers
 {
@@ -21,6 +25,38 @@ namespace SistemaTurnos.WebApplication.WebApi.Controllers
                 Subject = "Sistema de turnos",
                 Message = "<strong>MENSAJE DE PRUEBA CON ESTILOS EN HTML</strong>"
             });
+        }
+
+        [HttpPost]
+        public void Pay()
+        {
+            SDK.ClientId = "2128552166781000";
+            SDK.ClientSecret = "xt23Yx9BO3wqXO26aHWlzxvTuw7vFo6G";
+
+            // Create a preference object
+            Preference preference = new Preference();
+            preference.Items.Add(
+              new Item()
+              {
+                  Id = "1234",
+                  Title = "Enormous Iron Shirt",
+                  Quantity = 7,
+                  CurrencyId = CurrencyId.ARS,
+                  UnitPrice = 10
+              }
+            );
+
+            // Setting a payer object as value for Payer property
+            preference.Payer = new Payer()
+            {
+                Email = "christian.russo8@gmail.com"
+            };
+
+            // Save and posting preference
+            preference.Save();
+
+            var asd = preference.InitPoint;
+            asd = "ESAA";
         }
     }
 }
