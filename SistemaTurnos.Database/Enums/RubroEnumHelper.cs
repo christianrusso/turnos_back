ef376ead1a8aac2e009059ddeaa97bcf89f3842c
leandrobono <leandrobono85@gmail.com>
2019-02-04 11:45:38 -0300

    Fix enum

--- SistemaTurnos.Database/Enums/RubroEnumHelper.cs
+++ SistemaTurnos.Database/Enums/RubroEnumHelper.cs
@@ -6,6 +6,8 @@
         {
             if (id == 1) return RubroEnum.Clinic;
             if (id == 2) return RubroEnum.Hairdressing;
+            if (id == 3) return RubroEnum.Babershop;
+            if (id == 4) return RubroEnum.Esthetic;
 
             return RubroEnum.Unknown;
         }
