8ed752cfdfaa32e0ea1ef68dfd6e80d81e6ea8eb
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2019-03-27 17:15:12 -0300

    cambio el texto de todo lugar donde este el envio de email

--- SistemaTurnos.WebApplication/WebApi/Services/EmailService.cs
+++ SistemaTurnos.WebApplication/WebApi/Services/EmailService.cs
@@ -12,13 +12,15 @@ namespace SistemaTurnos.WebApplication.WebApi.Services
 
         public async void Send(EmailDto email)
         {
-            var from = new EmailAddress(email.From);
-            var subject = email.Subject;
-            var to = email.To.Select(t => new EmailAddress(t)).ToList();
-            var plainTextContent = "";
-            var htmlContent = email.Message;
-            var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            if(email.To[0].Contains("@")){
+                var from = new EmailAddress(email.From);
+                var subject = email.Subject;
+                var to = email.To.Select(t => new EmailAddress(t)).ToList();
+                var plainTextContent = "";
+                var htmlContent = email.Message;
+                var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
         }
     }
 }
